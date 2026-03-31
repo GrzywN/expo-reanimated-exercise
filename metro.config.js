4c915a020615e14be3879f88227c53ce4419ce79
Karol Binkowski <karolbinkowski3@proton.me>
2026-03-31 10:13:10 +0200

    feat: add react-native-svg-transformer package

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,19 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+const { transformer, resolver } = config;
+
+config.transformer = {
+  ...transformer,
+  babelTransformerPath: require.resolve('react-native-svg-transformer'),
+};
+
+config.resolver = {
+  ...resolver,
+  assetExts: resolver.assetExts.filter(ext => ext !== 'svg'),
+  sourceExts: [...resolver.sourceExts, 'svg'],
+};
+
 // Allow .gpx files to be bundled as static assets (loaded via expo-asset + fetch)
 config.resolver.assetExts.push('gpx');
 
